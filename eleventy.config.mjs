a5d9ab4e1124466511dd1df2f498e687af849544
James A. Duncan <james@kinburnstreet.consulting>
2025-07-15 10:50:50 +0100

    Sort the schemas by schema name

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,10 @@
 export default async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/styles.css");
+
+    eleventyConfig.addFilter("sortBySchemaName", (values) => {
+        let vals = [...values];
+        return vals.sort((a, b) => { return a.data.schematype.toLowerCase().localeCompare(b.data.schematype.toLowerCase()) });
+    });
 };
 
 export const config = {
